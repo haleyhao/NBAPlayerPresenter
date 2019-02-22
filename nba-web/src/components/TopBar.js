9f958ea5a5f5f1012bee141cf81115c0b971aa3a
Hao Zhang <haohaozhang0128@gmail.com>
2019-02-22 12:36:29 -0600

    display profile and stats

--- nba-web/src/components/TopBar.js
+++ nba-web/src/components/TopBar.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import nba from "../assets/images/nba.svg";
+import logo from '../assets/images/logo.svg';
 
 export class TopBar extends React.Component {
     render() {
         return (
-            <div>
-                <header className="App-header">
-                    <img src={nba} className="App-logo" alt="logo"/>
-                </header>
-            </div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
         );
     }
 }
